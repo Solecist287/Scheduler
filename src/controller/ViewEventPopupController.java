2127b7cb9a2a3b75a748fc8ccd36efd254b7782f
Justin <jayscro287@gmail.com>
2019-06-10 22:22:39 -0400

    implemented way to display event info from the vieweventpopup

--- src/controller/ViewEventPopupController.java
+++ src/controller/ViewEventPopupController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import javafx.fxml.FXML;
@@ -10,15 +13,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Event;
+import model.Timetable;
 
 public class ViewEventPopupController {
 	@FXML
 	ScrollPane infoScrollPane;
 	@FXML
-	Label infoLabel;
+	TextFlow eventInfo;
 	@FXML
 	Button modifyButton, removeButton;
 	
@@ -29,8 +35,50 @@ public class ViewEventPopupController {
 		this.primaryStage = primaryStage;
 		this.e = e;
 		this.events = events;
+		Text titleText = new Text(e.getTitle() + "\n");
+		titleText.setStyle("-fx-font-weight: bold;" + "-fx-font-size: 25;");
+		Text descriptionText = new Text(e.getDescription() + "\n");
+		descriptionText.setStyle("-fx-font-size: 12;");
+		//here we goooo
+		Text timeframeText = new Text(getTimeframeText());
+		timeframeText.setStyle("-fx-font-size: 12;");
+		
+		//add all text bois to textflow
+		eventInfo.setPrefWidth(Timetable.WIDTH/2);//should be half of timetable width?
+		eventInfo.setPrefHeight(Timetable.WIDTH/4);
+		eventInfo.getChildren().addAll(titleText,descriptionText,timeframeText);
 	}
 	//remove previous event, add new one with modified fields
+	private String getTimeframeText() {
+		LocalDateTime startDateTime = e.getStartDateTime();
+		LocalDate startDate = startDateTime.toLocalDate();
+		LocalTime startTime = startDateTime.toLocalTime();
+		LocalDateTime endDateTime = e.getEndDateTime();
+		LocalDate endDate = endDateTime.toLocalDate();
+		LocalTime endTime = endDateTime.toLocalTime();
+		if (startDate.equals(endDate)) {//same day
+			String dayOfWeek = startDate.getDayOfWeek().toString().toLowerCase();
+			return dayOfWeek.substring(0,1).toUpperCase() + dayOfWeek.substring(1) 
+					+ ", " + startDate + " " + formatTime(startTime) + "-" + formatTime(endTime);
+		}else {//different days
+			return startDate + ", " + formatTime(startTime) + "-" + endDate + ", " + formatTime(endTime);
+		}
+	}
+	private String getPeriod(int militaryHour) {
+		return militaryHour<12 ? "am" : "pm";
+	}
+	private int militaryToTwelveHour(int militaryHour) {
+		return (militaryHour%12==0) ? 12 : militaryHour%12;//converts hour from military to 12-hour time
+	}
+	//time => hour:min am/pm
+	private String formatTime(LocalTime t) {
+		int militaryHour = t.getHour();
+		int hour = militaryToTwelveHour(militaryHour);
+		String period = getPeriod(militaryHour);
+		String originalTime = t.toString();
+		return hour + originalTime.substring(2) + period;
+	}
+	
 	public void modifyEvent() {
 		//create popup stage
 		Stage popupStage = new Stage();
@@ -62,4 +110,5 @@ public class ViewEventPopupController {
 		events.remove(e);
 		primaryStage.close();
 	}
+	
 }
