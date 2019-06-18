f8e370d70142ccab6bbc1d7989f729bc9c3cdd15
Justin <jayscro287@gmail.com>
2019-06-18 19:30:20 -0400

    moved timeslot into hourlytimetable as inner class

--- src/model/HourlyTimetable.java
+++ src/model/HourlyTimetable.java
@@ -1,6 +1,9 @@
 package model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -10,9 +13,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public abstract class HourlyTimetable extends Timetable {
@@ -151,5 +156,42 @@ public abstract class HourlyTimetable extends Timetable {
 			hourlyGrid.getChildren().remove(timeslots.get(i).getView());
 		}
 		timeslots.clear();
+	}
+	//structure used to portray events on daytimetable and weektimetable
+	public class Timeslot {
+		private LocalDateTime startDateTime, endDateTime;
+		private Event e;
+		private HBox view;
+		private Label l;
+		private final int MIN_EVENT_TIME = 35;
+		public Timeslot(Event e, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+			this.e = e;
+			this.startDateTime = startDateTime;
+			this.endDateTime = endDateTime;
+			Color c = e.getBackgroundColor();
+			view = new HBox();
+			view.setStyle("-fx-background-color: rgb(" + c.getRed()*255 + "," + c.getGreen()*255 + "," + c.getBlue()*255 + ");"
+					+ "-fx-border-color: black;" + "-fx-font-size: 12");
+			//make sure event is at least minimum event time length (30 min)
+			if (startDateTime.until(e.getEndDateTime(), ChronoUnit.MINUTES) >= MIN_EVENT_TIME) {
+				l = new Label(e.getTitle() + "\n" 
+						+ TimeUtilities.formatTimeRange(e.getStartDateTime().toLocalTime(), e.getEndDateTime().toLocalTime()));
+				view.getChildren().add(l);
+			}
+		}
+		public Event getEvent() {
+			return e;
+		}
+		public HBox getView() {
+			return view;
+		}
+		public int getRowSpan() {
+			return (int)startDateTime.until(endDateTime, ChronoUnit.MINUTES)/5;
+		}
+		public int getRow() {
+			LocalTime startTime = startDateTime.toLocalTime();
+			return (startTime.getHour() * 60 + startTime.getMinute())/5;
+		}
+
 	}
 }
